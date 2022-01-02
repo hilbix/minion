78082e2afa760d12f795ef82b66ccc43fee631d9
Valentin Hilbig <webmaster@scylla-charybdis.com>
2022-01-02 19:40:23 +0100

    web/gh.js modified

--- web/gh.js
+++ web/gh.js
@@ -58,6 +58,10 @@ class Main
       e.text(': ');
       const i = e.INPUT.attr({placeholder:'GitHub PAT', title:'GitHub PAT, no spaces'});
       const b = e.BUTTON.text('store in localStorage').on('click', _ => { localStorage.setItem(this.tok, i.$value); this.expire(); this.run() });
+      e.DIV.text("Notes:")
+      e.DIV.text("This is tool to access GitHub API using OctoCat.  It needs a PAT to access your repos.  Be sure to limit access rights of that PAT (read-only is enough for now).");
+      e.DIV.text("Caching in Browser's localStore is used, as GitHub limits the number of requests.  The cache is not refreshed automatically.  Clear the cache (with the 'clear cache' button) if needed, else you might see stale data.");
+      e.DIV.text("Last tested 2022-01-02 with FF95, Chrome96, Edge96");
     }
   setup()
     {
@@ -117,8 +121,9 @@ class Main
     {
       const e = copyButton(r.TD, s).SPAN;
       if (s === void 0 || s === null)
-        return;
+        s = '';
       tooLong();
+      return e;
       function tooLong()
         {
           e.clr()
@@ -141,11 +146,20 @@ class Main
 	  this.td(r, k);
 	  this.td(r, localStorage[k]);
 	}
+      e.DIV.text("Notes:")
+      e.DIV.text("This shows the locally stored cache entries of this site.");
+      e.DIV.text("Some may be from other pages on this site.");
     }
   async fn_repos(e)
     {
       const repos = await this.paginate('/user/repos');
       const t = e.clr().TABLE;
+      t.TR
+       .TH.text('F').attr({title:'Link to the original if forked'}).$$
+       .TH.text('W').attr({title:'Link to the wiki'}).$$
+       .TH.text('repo').$$
+       .TH.text('title').$$
+       .TH.text('json');
       for (const k of repos)
         {
 	  const r = t.TR;
@@ -155,21 +169,32 @@ class Main
 	      const info = await this.get(`/repos/${k.full_name}`);
 	      const src = info.data?.source?.html_url;
 	      if (src)
-	        x.A.href(info.data.source.html_url).text('F')
+	        x.A.href(info.data.source.html_url).text('x')
 	      else
 	        x.A.href(info.data?.parent?.html_url).text('?')
 	    }
-          copyButton(r.TD, k.full_name).A.href(`${k.html_url}`).text(k.full_name);
-	  this.td(r, k.full_name);
+	  const w = r.TD;
+	  if (k.has_wiki)
+	    w.A.href(`${k.html_url}/wiki`).text('W');
+          copyButton(r.TD, k.full_name).A.href(k.html_url).text(k.full_name);
+//	  this.td(r, k.name);
 //	  this.td(r, k.owner.login);
-	  this.td(r, k.description, 60);
+	  this.td(r, k.description);
 	  this.td(r, JSON.stringify(k), 40);
 	}
+      e.DIV.text("Notes:")
+      e.DIV.text("This is a quick table of all repositories of you, your organizations and repositories you contribute to.");
     }
   async fn_keys(e)
     {
       const repos = await this.paginate('/user/repos');
       const t = e.clr().TABLE;
+      t.TR
+       .TH.text('name').$$
+       .TH.text('RW').attr({title:'read-write or read-only'}).$$
+       .TH.text('V').attr({title:'Verification status (I have no idea what this means)'}).$$
+       .TH.text('key-type').$$
+       .TH.text('list of repositories');
       const a = {};
       for (const k of repos)
         {
@@ -177,23 +202,33 @@ class Main
 	  const d = await this.paginate(u);
 	  for (const m of d)
 	    {
-	      const g = a[m.title] || (a[m.title]=[]);
+	      const w = `${m.title} ${m.read_only ? '0' : '1'}${m.verified ? '0' : '1'}${m.key.split(' ',1)[0]}`;
+	      const g = a[w] || (a[w]=[]);
 	      g.push(k);
+//	      t.TR.TD.text(JSON.stringify(m));
 	    }
 	}
-     let tmp;
+//     let tmp;
      for (const g in a)
        {
 	  const r = t.TR;
-	  this.td(r, g);
+	  const x = g.split(' ');
+	  const f = x.pop();
+	  this.td(r, x.join(' ')).$$.addclass('pre');
+	  r.TD.text(f[0]=='0' ? '-'  : 'x');
+	  r.TD.text(f[1]=='0' ? 'x'  : '-');
+	  r.TD.text(f.substr(2)).addclass('pre');
 	  const d = r.TD;
 	  for (const u of a[g])
 	    {
               copyButton(d, u.full_name).A.href(`${u.html_url}/settings/keys`).text(u.full_name);
-	      tmp = u;
+//	      tmp = u;
 	    }
        }
 //      e.PRE.text(JSON.stringify(tmp, void 0, 2));
+      e.DIV.text("Notes:")
+      e.DIV.text("This is a list of all deployment keys you have configured on GitHub.");
+      e.DIV.text("Grouped by the name you gave to the deployment key.");
     }
   };
 
