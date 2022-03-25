5b6c1183f2eca2b627e9e5a2c340d2dba6c32205
Valentin Hilbig <webmaster@scylla-charybdis.com>
2022-03-25 07:49:23 +0100

    web/minion.js added

---
+++ web/minion.js
@@ -0,0 +1,33 @@
+// Output Minion menu line
+
+'use strict';
+
+(_=>_())(()=>{
+
+const e = E.DIV.text('(menu loading)');
+E(document.currentScript).after(e);
+
+fetch('minion.txt ')
+.then(_ => { if (_.ok) return _.text(); throw `fetch failed: ${_.status} ${_.url}` })
+.then(menu =>
+  {
+    console.log(menu);
+    e.clr();
+    let f = '[ ';
+    for (const m of menu.split('\n'))
+      if (m)
+        {
+          const u = `${m}.html`;
+          const url = new URL(u, window.location.href);
+          if (window.location.href === url.href)
+            e.text(f, m);
+          else
+            e.text(f).A.href(u).text(m);
+          f = ' | ';
+        }
+    e.text(' ]');
+  })
+.catch(err => e.$text = `(ERROR: ${err})`)
+
+});
+
