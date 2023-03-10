e9e76d7474ea43bb8af4ae9aba3bdceaba129346
Valentin Hilbig <webmaster@scylla-charybdis.com>
2023-03-10 16:34:45 +0100

    web/broadcast.js modified

--- web/broadcast.js
+++ web/broadcast.js
@@ -16,24 +16,24 @@ class Main
     }
   show()
     {
-      const e = this.main.clr().DIV;
-      const t = e.BUTTON.text('send').on('click', _ => { this.send(m.$value) });
-      e.text(` (version ${this.v}`);
-      e.A.href('broadcast.html').target().text('open other window(s) in the same browser to recv/send broadcast');
-      e.BR;
-      const m = e.TEXTAREA.attr({placeholder:'Message'});
+      const e = this.main.clr();
+      e.DIV.text(` (version ${this.v} `).A.href('broadcast.html').target().text('open other window(s) in the same browser to recv/send broadcast');
+      const m = e.DIV.TEXTAREA.attr({placeholder:'Message',cols:80,rows:10}).onkey('keypress', { 'C_Enter C_NumpadEnter':_ => this.send(m.$value) && m.$.select() });
+      const t = e.DIV.BUTTON.text('send').on('click', _ => { this.send(m.$value) }).$$.text(' (CTRL+Enter to send)');
       e.HR;
-      e.text('Channel:');
-      const c = e.INPUT.attr({placeholder:'Broadcast Channel'});
-      const b = e.BUTTON.text('subscribe').on('click', _ => { this.sub(c.$value,_) });
-      this.l = e.SPAN;
+      const x = e.DIV;
+      x.text('Channel: ');
+      const c = x.INPUT.attr({placeholder:'Broadcast Channel'}).onkey('keypress', { 'Enter NumpadEnter':_ => this.sub(c.$value,_) && m.focus() });
+      const b = x.BUTTON.text('subscribe').on('click', _ => { this.sub(c.$value,_); m.focus() })
+
+      this.l = e.DIV;
       if (c.$text.trim()=='')
         c.focus();
       else
         {
-	  this.sub(c.$value);
+          this.sub(c.$value);
           m.focus();
-	}
+        }
     }
   sub(c)
     {
@@ -43,22 +43,30 @@ class Main
       this.c	= new BroadcastChannel(c);
       this.cn	= c;
       if (!this.c)
-        return this.l.clr().addclass('bgred').text(`failed to subscribe to: ${toJ(HE(c))}`);
-      this.l.clr().rmclass('bgred').text(`subscribed to: ${toJ(HE(c))}`);
+        return this.err(`failed to subscribe to: ${toJ(HE(c))}`);
       this.c.onmessage = _ => this.recv(_);
+      return this.prep(`subscribed to: ${toJ(HE(c))}`);
     }
   send(m)
     {
       if (!this.c)
-        return this.l.clr().addclass('bgred').text('please subscribe to a channel first');
+        return this.err('please subscribe to a channel first');
       CONSOLE('send', this.cn, m);
       this.c.postMessage(m);
-      this.l.prep(E.DIV.text(`sent: ${toJ(HE(m))}`));
+      return this.prep(`sent: ${toJ(HE(m))}`, m);
     }
   recv(m)
     {
       CONSOLE('recv', this.cn, m);
-      this.l.prep(E.DIV.text(`recv: ${toJ(HE(m.data))}`));
+      return this.prep(`recv: ${toJ(HE(m.data))}`, m);
+    }
+  err(t,m) { return this._prep(t,m,'bgred') }
+  prep(t,m) { return this._prep(t,m) }
+  _prep(t,m,c)
+    {
+      const e = E.DIV.text(t).if$(c,E.addclass,c);
+      this.l.prep(e);
+      return this;
     }
   };
 
