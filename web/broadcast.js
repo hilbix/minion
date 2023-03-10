4c4cec871f642ed85e61e5198e9a458d8f077174
Valentin Hilbig <webmaster@scylla-charybdis.com>
2023-03-10 17:43:21 +0100

    fixup! web/broadcast.js modified

--- web/broadcast.js
+++ web/broadcast.js
@@ -18,14 +18,18 @@ class Main
     {
       const e = this.main.clr();
       e.DIV.text(` (version ${this.v} `).A.href('broadcast.html').target().text('open other window(s) in the same browser to recv/send broadcast');
-      const m = e.DIV.TEXTAREA.attr({placeholder:'Message',cols:80,rows:10}).onkey('keypress', { 'C_Enter C_NumpadEnter':_ => this.send(m.$value) && m.$.select() });
-      const t = e.DIV.BUTTON.text('send').on('click', _ => { this.send(m.$value) }).$$.text(' (CTRL+Enter to send)');
+      const m = e.DIV.TEXTAREA.attr({placeholder:'Message',cols:80,rows:10}).onkey('keydown',
+        { 'C_Enter C_NumpadEnter':_ => this.send(m.$value) && m.$.select()
+        , 'Escape':_ => m.editval('\t')
+        })
+      const t = e.DIV.BUTTON.text('send').on('click', _ => { this.send(m.$value) }).$$.text(' (CTRL+Enter to send) (ESC key inserts a TAB)');
       e.HR;
       const x = e.DIV;
       x.text('Channel: ');
-      const c = x.INPUT.attr({placeholder:'Broadcast Channel'}).onkey('keypress', { 'Enter NumpadEnter':_ => this.sub(c.$value,_) && m.focus() });
+      const c = x.INPUT.attr({placeholder:'Broadcast Channel'}).onkey('keydown', { 'Enter NumpadEnter':_ => this.sub(c.$value,_) && m.focus() });
       const b = x.BUTTON.text('subscribe').on('click', _ => { this.sub(c.$value,_); m.focus() })
-
+      x.text(' | ').BUTTON.text('(clear output below)').on('click', _ => { this.l.clr() })
+      e.HR;
       this.l = e.DIV;
       if (c.$text.trim()=='')
         c.focus();
@@ -39,13 +43,16 @@ class Main
     {
       CONSOLE('sub', c);
       if (this.c)
-        this.c.close();
+        {
+          this.c.close();
+          this.prep(`unsubscribed from: ${toJ(this.cn)}`);
+        }
       this.c	= new BroadcastChannel(c);
       this.cn	= c;
       if (!this.c)
-        return this.err(`failed to subscribe to: ${toJ(HE(c))}`);
+        return this.err(`failed to subscribe to: ${toJ(c)}`);
       this.c.onmessage = _ => this.recv(_);
-      return this.prep(`subscribed to: ${toJ(HE(c))}`);
+      return this.prep(`subscribed to: ${toJ(c)}`);
     }
   send(m)
     {
@@ -53,12 +60,12 @@ class Main
         return this.err('please subscribe to a channel first');
       CONSOLE('send', this.cn, m);
       this.c.postMessage(m);
-      return this.prep(`sent: ${toJ(HE(m))}`, m);
+      return this.prep(`sent: ${toJ(m)}`, m);
     }
   recv(m)
     {
       CONSOLE('recv', this.cn, m);
-      return this.prep(`recv: ${toJ(HE(m.data))}`, m);
+      return this.prep(`recv: ${toJ(m.data)}`, m);
     }
   err(t,m) { return this._prep(t,m,'bgred') }
   prep(t,m) { return this._prep(t,m) }
