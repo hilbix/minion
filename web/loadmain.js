28a43c9e0a7499a2e6b4015257aa12254a9de835
Valentin Hilbig <webmaster@scylla-charybdis.com>
2023-07-05 05:15:48 +0200

    web/loadmain.js changed

--- web/loadmain.js
+++ web/loadmain.js
@@ -1,17 +1,1 @@
-'use strict';
-
-const _ = E('main').clr().text('initializing');
-try {
-  const { default:modules } = await import('./loadmodule')
-  console.log(_.$text = 'loading');
-  const main = await modules.main;
-  console.log(_.$text = 'loaded');
-  if (!main.Main) throw 'missing class Main';
-  const run = new main.Main(_);
-  if (!run.main) throw 'missing classfunction Main.main()';
-  run.main();
-} catch (e) {
-  _.$text = `load failed: ${e}`;
-  throw e;
-}
-
+../js/loadmain.js
