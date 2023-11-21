8aa4ddf68564da0205610ff5a14818c7942d9101
Valentin Hilbig <webmaster@scylla-charybdis.com>
2023-11-21 13:50:02 +0100

    web/minion.js modified

--- web/minion.js
+++ web/minion.js
@@ -11,27 +11,51 @@ if (c.PREV.Dataset('minion')!=='menu')
 const m = c.PREV.clr();
 console.log(m.$);
 
-m.SPAN.text('[ ').a('/', 'Home').text(' ] ');
+const t = m.SPAN.text('[ ').a('..', 'Back').text(' ] ');
 const e = m.SPAN.text('(menu loading)');
 m.text(' ').SPAN.id('menu');
 
-async function menu(cache)
+function menu(cache)
   {
-    const _ = await fetch('minion.txt', {cache});
-    if (!_.ok) throw `fetch(${cache}) failed: ${_.status} ${_.url}`;
-    const menu = await _.text();
+    const p = c.$.src.split('.');
+    p.pop();
+    p.push('txt');
+    return Promise.all(
+      [ mkmenu(t, '../back.txt', cache, ' Back', '../')
+      , mkmenu(e, p.join('.'), cache)
+      ]);
+  }
+async function mkmenu(e, url, cache, def, path)
+  {
+    let tickets;
+
+    const _ = await fetch(url, {cache});
+    console.log({_});
+    if (!_.ok && !def) throw `fetch(${cache}) failed: ${_.status} ${_.url}`;
+    const menu = _.ok ? await _.text() : def;
 //    console.log(menu);
     e.clr();
     let f = '[ ';
     for (const m of menu.split('\n'))
       if (m)
         {
-          const u = `${m}.html`;
+          const s = m.split(/\s+/);
+          const s0 = s.shift();
+          if (s0 === '#ticket')
+            {
+              tickets	= s[0];
+              continue;
+            }
+
+          console.log(s0, toJ(s), tickets);
+          const tick	= tickets ? s.shift().split(',') : [];
+          const u = !s0 || s0.includes('.') ? `${path||''}${s0}` : `${path||''}${s0}.html`;
+
           const url = new URL(u, window.location.href);
           if (window.location.href === url.href)
             e.text(f, m);
           else
-            e.text(f).A.href(u).text(m);
+            e.text(f).A.href(u).text(s.length ? s.join(' ') : s0);
           f = ' | ';
         }
     e.text(' ]');
@@ -39,12 +63,15 @@ async function menu(cache)
 let errs=0;
 function err(_)
   {
+    console.error('ERR', _);
     e.clr().SPAN.text(`(ERROR #${++errs}) ${_}`).on('click', ()=>menu('reload').catch(err))
   }
+
+console.log('MMMM', c.$);
 
 menu('force-cache')
 .then(() => menu('no-cache'))
 .catch(err)
-.finally(() => e.text(' ').A.href('https://github.com/hilbix/minion/tree/master/web').text('source'))
+.finally(() => c.$.src.includes('minion') && e.text(' ').A.href('https://github.com/hilbix/minion/tree/master/web').text('source'))
 });
 
