7271c75e25eeb980d7f8351c26a992b1122226ac
Valentin Hilbig <webmaster@scylla-charybdis.com>
2023-11-26 18:55:36 +0100

    web/_.js modified

--- web/_.js
+++ web/_.js
@@ -7,7 +7,7 @@ async function tooLong(e,s,max)
       max	= parseInt(await sessionStorage.getItem('max'));
       if (!max || max<1) max=80;
     }
-  const short = () => { e.clr().text(s.substr(0,max-1)).if(s.length>=max, () => e.A.text('...').on('click', long)) };
+  const short = () => { e.clr().text(s.substr(0,max-1)).if(s.length>=max+4, () => e.A.text('...').on('click', long)) };
   const long  = () => { e.clr().text(s).on('click', short) };
   short();
 }
